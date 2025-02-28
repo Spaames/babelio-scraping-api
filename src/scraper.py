d7b8f0e8b7cb6fa75497d13e7a503b3a34325849
Rémi Dupuy <rdu@mac.home>
2025-02-28 15:30:37 +0100

    added author, img and link for each books searched

--- src/scraper.py
+++ src/scraper.py
@@ -15,8 +15,15 @@ def scrape_books_data(search_value):
 
     for book_div in soup.find_all('div', {'class': 'cr_carte'}):
         try:
+            img_src = book_div.find('img', {'class': 'cr_image'}).get('src')
+            img_true_path = f"https://www.babelio.com{img_src}"
+            book_href = book_div.find('a', {'class': 'titre1'}).get('href')
+            book_true_path = f"https://www.babelio.com{book_href}"
             book_info = {
-                'title': book_div.find('a', {'class': 'titre1'}).text.strip()
+                'title': book_div.find('a', {'class': 'titre1'}).text.strip(),
+                'author': book_div.find('a', {'class': 'libelle'}).text.strip(),
+                'img': img_true_path,
+                'book_url': book_true_path,
             }
             books.append(book_info)
         except AttributeError:
