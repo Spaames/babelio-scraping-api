cbcd06d8b6c2bc4eafcbaca1c439b512efa8bc37
Rémi Dupuy <rdu@mac.home>
2025-02-28 16:28:20 +0100

    new endpoint -> book (all data of final book page)

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from scraper import scrape_books_data
+from scraper import scrape_books_list, scrape_book
 
 bp = Blueprint('api', __name__)
 
@@ -10,10 +10,22 @@ def search():
     if not search_value:
         return jsonify({"error": "No search value provided" }), 400
 
-    book_data = scrape_books_data(search_value)
+    book_data = scrape_books_list(search_value)
 
     if book_data:
         return jsonify(book_data), 200
     else:
         return jsonify({"error": "Book not found" }), 400
 
+@bp.route('/book', methods=['GET'])
+def get_book():
+    book_url = request.args.get('query')
+    if not book_url:
+        return jsonify({"error": "No book url provided" }), 400
+
+    book_data = scrape_book(book_url)
+
+    if book_data:
+        return jsonify(book_data), 200
+    else:
+        return jsonify({"error": "Book not found" }), 400
